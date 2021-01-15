d2558d5905f883808f198c3af9b5a465a044b24f
geeklili <netgeek@163.com>
2021-01-15 15:54:35 +0800

    add run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,36 @@
-nohup python3 run.py &
+#!/bin/bash
+
+workdir=/root/Shared_Photo_Albums
+. venv/bin/activate
+
+daemon_start(){
+	cd $workdir
+	nohup python3 run.py&
+	echo"Serverstarted."
+}
+
+daemon_stop(){
+	pid=`ps -ef | grep 'python3 run.py' | awk 'BEGIN{ ORS="," }{ print $2 }'`
+	arr=(`echo ${pid} | tr ',' ' '`)
+	echo ${arr[0]}
+	kill -9 ${arr[0]}
+	sleep 2
+	echo "Serverkilled."
+}
+
+case "$1" in
+	--start)
+		daemon_start
+		;;
+	--stop)
+		daemon_stop
+		;;
+	--restart)
+		daemon_stop
+		daemon_start
+		;;
+	*)
+		echo"Usage: ./run.sh {start|stop|restart}"
+		exit 1
+esac
+exit 0
